284a034497dbaf583e19d7263a20913702530685
Ian Fischer <iafi3572@colorado.edu>
2025-01-23 20:59:40 -0700

    finished

--- Homework/homework1/Problem3.py
+++ Homework/homework1/Problem3.py
@@ -2,12 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def f(x):
-    return abs((np.sin(x)-17*x*np.sin(x)+3*np.cos(x)-9*(x**2)*np.cos(x)+(x**3)*np.sin(x)))/48
-xvals = np.linspace(-1,1,100)
+    return abs((1-17*x+ x**3 )*np.cos(x) + (3- 9* x**2)*np.cos(x))/48
+xvals = np.linspace(0,.5,1000)
 plt.plot(xvals,f(xvals))
 plt.savefig('Problem3')
 print(max(f(xvals)))
 
 def error():
-    return abs(np.cos(.5)*(1-1/2+1/8) -1 - 1/2 +1/8)
+    return abs(np.cos(.5)*(1+1/2+1/8) -1 - 1/2 +1/8)
 print(error())
