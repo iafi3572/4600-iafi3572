cff0d6fd540708c7d182ec51c3b4e90fd8d5ffc3
Ian Fischer <iafi3572@colorado.edu>
2025-01-30 13:08:13 -0700

    problem3 done

--- Homework/homework2/problem3.py
+++ Homework/homework2/problem3.py
@@ -6,3 +6,19 @@ def alg(x):
 print(alg(0))
 print(alg(9.999999995000000 * 10**(-10)))
 print(np.expm1(9.999999995000000 * 10**(-10)))
+
+def partd():
+    x = 9.999999995000000 * 10 **(-10)
+    E = lambda n : (np.e**x * (x)**(n+1))/ abs(np.e**x -1)*(math.factorial(n+1))
+    n =1
+    while E(n) > 10**(-16):
+        n = n+1
+    return n
+print(partd())
+
+def parte():
+    x = 9.999999995000000 * 10 **(-10)
+    f = lambda x :np.e**x -1
+    aprx = lambda x: x+x**2/2
+    return abs( f(x) - aprx(x) )
+print(parte())
