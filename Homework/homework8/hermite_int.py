30dca5bdb6a3cc76a8ff4ad5ad5b916c447b5557
Ian Fischer <iafi3572@colorado.edu>
2025-03-12 13:09:13 -0600

    problem1 and 2 i hope

--- Homework/homework8/hermite_int.py
+++ Homework/homework8/hermite_int.py
@@ -3,18 +3,22 @@ import numpy as np
 from numpy.linalg import inv
 import math
 epsilon = np.finfo(float).eps
-def driver(N,verb1 =True, verb2 = True):
+def driver(N,verb1 =True, verb2 = True,verb3 = True,verb4 = True):
 
     f = lambda x: 1./(1.+x**2)
     fp = lambda x: -2*x/(1.+x**2)**2
-    
+    fp0 = fp(-5)
+    fpN = fp(5)
     ''' interval'''
     a = -5
     b = 5
    
     ''' create equispaced interpolation nodes'''
     xint = np.linspace(a,b,N+1)
-    xint1 = np.array([0.5 * (b - a) * np.cos((2 * i + 1) * np.pi / (2 * (N + 1))) + 0.5 * (a + b) for i in range(N+1)])
+    xint1 = np.array([
+        0.5 * (b + a) + 0.5 * (b - a) * np.cos((N - i) * np.pi / N) 
+        for i in range(N+1)
+    ])    
     ''' create interpolation data'''
     yint = np.zeros(N+1)
     yint1 = np.zeros(N+1)
@@ -32,8 +36,8 @@ def driver(N,verb1 =True, verb2 = True):
     xeval = np.linspace(a,b,Neval+1)
     (M,C,D) = create_natural_spline(yint,xint,N)
     (M1,C1,D1) = create_natural_spline(yint1,xint1,N)
-    (Ma,Ca,Da) = create_clamped_spline(yint,xint,N)
-    (Ma1,Ca1,Da1) = create_clamped_spline(yint1,xint1,N)
+    (Ma,Ca,Da) = create_clamped_spline(yint,xint,N,fp0,fpN)
+    (Ma1,Ca1,Da1) = create_clamped_spline(yint1,xint1,N,fp0,fpN)
     yevalL = np.zeros(Neval+1)
     yevalH = np.zeros(Neval+1)
     yevalC = eval_cubic_spline(xeval,Neval,xint,N,M,C,D)
@@ -55,24 +59,40 @@ def driver(N,verb1 =True, verb2 = True):
     
     if verb1:
         plt.figure()
+        plt.title("Lagrange and Hermite\nEquipsaced Nodes N = {N}".format(N=N))
         plt.plot(xeval,fex,label = "f(x)")
         plt.plot(xeval,yevalL,label='Lagrange') 
         plt.plot(xeval,yevalH,label='Hermite')
-        plt.plot(xeval,yevalC,label='Natural Spline') 
-        plt.plot(xeval,yevalD,label='Clamped Spline') 
         plt.legend()
         plt.semilogy()
-        plt.savefig("problem1.{N}.png".format(N=N))
+        plt.savefig("problem1.{N}LnH.png".format(N=N))
     if verb2:
         plt.figure()
+        plt.title("Splines\nEquipsaced Nodes N = {N}".format(N=N))
+        plt.plot(xeval,fex,label = "f(x)")
+        plt.plot(xeval,yevalC,label='Natural Spline',color = "purple") 
+        plt.plot(xeval,yevalD,label='Clamped Spline',color = "red") 
+        plt.legend()
+        plt.semilogy()
+        plt.savefig("problem1.{N}spline.png".format(N=N))
+    if verb3:
+        plt.figure()
+        plt.title("Lagrange and Hermite\nChebychev Nodes N = {N}".format(N=N))
         plt.plot(xeval,fex,label = "f(x)")
         plt.plot(xeval,yevalL1,label='Lagrange') 
         plt.plot(xeval,yevalH1,label='Hermite')
-        plt.plot(xeval,yevalC1,label='Natural Spline') 
-        plt.plot(xeval,yevalD1,label='Clamped Spline')  #THIS BROKE
         plt.legend()
         plt.semilogy()
-        plt.savefig("problem2.{N}.png".format(N=N))
+        plt.savefig("problem2.{N}LnH.png".format(N=N))
+    if verb4:
+        plt.figure()
+        plt.title("Splines\nChebychev Nodes N = {N}".format(N=N))
+        plt.plot(xeval,fex,label = "f(x)")
+        plt.plot(xeval,yevalC1,label='Natural Spline',color = "purple") 
+        plt.plot(xeval,yevalD1,label='Clamped Spline',color = "red") 
+        plt.legend()
+        plt.semilogy()
+        plt.savefig("problem2.{N}spline.png".format(N=N))
     ''' 
     errL = abs(yevalL-fex)
     errH = abs(yevalH-fex)
@@ -159,11 +179,10 @@ def create_natural_spline(yint,xint,N):
        A[j][j] = (h[j]+h[j-1])/3 
        A[j][j+1] = h[j]/6
     A[N][N] = 1
-
     Ainv = inv(A)
     
-    M  = Ainv.dot(b)
-
+    #M  = Ainv.dot(b)
+    M = np.linalg.solve(A, b) 
 #  Create the linear coefficients
     C = np.zeros(N)
     D = np.zeros(N)
@@ -172,40 +191,48 @@ def create_natural_spline(yint,xint,N):
        D[j] = yint[j+1]/h[j]-h[j]*M[j+1]/6
     return(M,C,D)
 
-def create_clamped_spline(yint,xint,N):
-
-#    create the right  hand side for the linear system
-    b = np.zeros(N+1)
-#  vector values
-    h = np.zeros(N+1)  
-    for i in range(1,N):
-       hi = xint[i]-xint[i-1]
-       hip = xint[i+1] - xint[i]
-       b[i] = (yint[i+1]-yint[i])/hip - (yint[i]-yint[i-1])/hi
-       h[i-1] = hi
-       h[i] = hip
-
-#  create matrix so you can solve for the M values
-# This is made by filling one row at a time 
-    A = np.zeros((N+1,N+1))
-    A[0][0] = 1.0
-    for j in range(1,N):
-       A[j][j-1] = h[j-1]/6
-       A[j][j] = (h[j]+h[j-1])/3 
-       A[j][j+1] = h[j]/6
-    A[N][N] = 1
-
-    Ainv = inv(A)
+def create_clamped_spline(yint, xint, N, fp0, fpN):
+    # Create the right-hand side for the linear system
+    b = np.zeros(N + 1)
+    h = np.zeros(N + 1)  # Step sizes
     
-    M  = Ainv.dot(b)
+    for i in range(1, N):
+        hi = xint[i] - xint[i-1]
+        hip = xint[i+1] - xint[i]
+        b[i] = (yint[i+1] - yint[i]) / hip - (yint[i] - yint[i-1]) / hi
+        h[i-1] = hi
+        h[i] = hip
+    
+    # added for clamped boundary conditions
+    h[N] = xint[N] - xint[N-1]
+    b[0] = (yint[1] - yint[0]) / h[0] - fp0
+    b[N] = fpN - (yint[N] - yint[N-1]) / h[N-1]
+    # Construct the matrix A for solving M values
+    A = np.zeros((N + 1, N + 1))
+    # changed for clamped condition
+    A[0][0] = 2 / h[0]  
+    A[0][1] = 1 / h[0]
+    
+    for j in range(1, N):
+        A[j][j-1] = h[j-1] / 6
+        A[j][j] = (h[j] + h[j-1]) / 3
+        A[j][j+1] = h[j] / 6
+    # changed for clamped
+    A[N][N-1] = 1 / h[N-1]
+    A[N][N] = 2 / h[N-1]  
 
-#  Create the linear coefficients
+    # Solve for M
+    Ainv = inv(A)
+    #M = Ainv.dot(b)
+    M = np.linalg.solve(A, b) 
+    # Create the linear coefficients
     C = np.zeros(N)
     D = np.zeros(N)
     for j in range(N):
-       C[j] = yint[j]/h[j]-h[j]*M[j]/6
-       D[j] = yint[j+1]/h[j]-h[j]*M[j+1]/6
-    return(M,C,D) 
+        C[j] = yint[j] / h[j] - h[j] * M[j] / 6
+        D[j] = yint[j+1] / h[j] - h[j] * M[j+1] / 6
+    
+    return M, C, D
 
 def eval_local_spline(xeval,xi,xip,Mi,Mip,C,D):
 # Evaluates the local spline as defined in class
@@ -238,11 +265,12 @@ def  eval_cubic_spline(xeval,Neval,xint,Nint,M,C,D):
         yeval[ind] = yloc
 
     return(yeval)
-           
+     
 driver(5)  
-driver(10)
-driver(15)
-driver(20)             
+driver(10)  
+driver(15)  
+driver(20)  
+         
 
    
    
