a508ad3b5c96b16bd93d6a4ab256bf5f486340dc
Ian Fischer <iafi3572@colorado.edu>
2025-03-11 19:01:19 -0600

    more

---
+++ Homework/homework8/hermite_int.py
@@ -0,0 +1,243 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy.linalg import inv
+import math
+epsilon = np.finfo(float).eps
+def driver(N,verb1 =True, verb2 = True):
+
+    f = lambda x: 1./(1.+x**2)
+    fp = lambda x: -2*x/(1.+x**2)**2
+    
+    ''' interval'''
+    a = -5
+    b = 5
+   
+    ''' create equispaced interpolation nodes'''
+    xint = np.linspace(a,b,N+1)
+    xint1 = np.array([0.5 * (b - a) * np.cos((2 * i + 1) * np.pi / (2 * (N + 1))) + 0.5 * (a + b) for i in range(N+1)])
+    ''' create interpolation data'''
+    yint = np.zeros(N+1)
+    yint1 = np.zeros(N+1)
+    ypint = np.zeros(N+1)
+    ypint1 = np.zeros(N+1)
+
+    for jj in range(N+1):
+        yint[jj] = f(xint[jj])
+        yint1[jj] = f(xint1[jj])
+        ypint[jj] = fp(xint[jj])
+        ypint1[jj] = fp(xint1[jj])
+    ''' create points for evaluating the Lagrange interpolating polynomial'''
+    
+    Neval = 1000
+    xeval = np.linspace(a,b,Neval+1)
+    (M,C,D) = create_natural_spline(yint,xint,N)
+    (M1,C1,D1) = create_natural_spline(yint1,xint1,N)
+    yevalL = np.zeros(Neval+1)
+    yevalH = np.zeros(Neval+1)
+    yevalC = eval_cubic_spline(xeval,Neval,xint,N,M,C,D)
+    yevalL1 = np.zeros(Neval+1)
+    yevalH1 = np.zeros(Neval+1)
+    yevalC1 = eval_cubic_spline(xeval,Neval,xint1,N,M1,C1,D1)
+    for kk in range(Neval+1):
+      yevalL[kk] = eval_lagrange(xeval[kk],xint,yint,N)
+      yevalH[kk] = eval_hermite(xeval[kk],xint,yint,ypint,N)
+      yevalL1[kk] = eval_lagrange(xeval[kk],xint1,yint1,N)
+      yevalH1[kk] = eval_hermite(xeval[kk],xint1,yint1,ypint1,N)
+
+    ''' create vector with exact values'''
+    fex = np.zeros(Neval+1)
+    for kk in range(Neval+1):
+        fex[kk] = f(xeval[kk])
+    
+    if verb1:
+        plt.figure()
+        plt.plot(xeval,fex,label = "f(x)")
+        plt.plot(xeval,yevalL,label='Lagrange') 
+        plt.plot(xeval,yevalH,label='Hermite')
+        plt.plot(xeval,yevalC,label='Natural spline') 
+        plt.legend()
+        plt.semilogy()
+        plt.savefig("problem1.{N}.png".format(N=N))
+    if verb2:
+        plt.figure()
+        plt.plot(xeval,fex,label = "f(x)")
+        plt.plot(xeval,yevalL1,label='Lagrange') 
+        plt.plot(xeval,yevalH1,label='Hermite')
+        plt.plot(xeval,yevalC1,label='Natural spline') 
+        plt.legend()
+        plt.semilogy()
+        plt.savefig("problem2.{N}.png".format(N=N))
+    ''' 
+    errL = abs(yevalL-fex)
+    errH = abs(yevalH-fex)
+    plt.figure()
+    plt.semilogy(xeval,errL,'bs--',label='Lagrange')
+    plt.semilogy(xeval,errH,'c.--',label='Hermite')
+    plt.show()            
+    '''
+
+def eval_hermite(xeval,xint,yint,ypint,N):
+
+    ''' Evaluate all Lagrange polynomials'''
+
+    lj = np.ones(N+1)
+    for count in range(N+1):
+       for jj in range(N+1):
+           if (jj != count):
+              lj[count] = lj[count]*(xeval - xint[jj])/(xint[count]-xint[jj])
+              
+              
+    ''' Construct the l_j'(x_j)'''
+    lpj = np.zeros(N+1)
+#    lpj2 = np.ones(N+1)
+    for count in range(N+1):
+       for jj in range(N+1):
+           if (jj != count):
+#              lpj2[count] = lpj2[count]*(xint[count] - xint[jj])
+              lpj[count] = lpj[count]+ 1./(xint[count] - xint[jj])
+
+    yeval = 0.
+     
+    for jj in range(N+1):
+       Qj = (1.-2.*(xeval-xint[jj])*lpj[jj])*lj[jj]**2
+       Rj = (xeval-xint[jj])*lj[jj]**2
+#       if (jj == 0):
+#         print(Qj)
+         
+#         print(Rj)
+#         print(Qj)
+#         print(xeval)
+ #        return
+       yeval = yeval + yint[jj]*Qj+ypint[jj]*Rj
+       
+    return(yeval)
+       
+
+
+def eval_lagrange(xeval,xint,yint,N):
+
+    lj = np.ones(N+1)
+    
+    for count in range(N+1):
+       for jj in range(N+1):
+           if (jj != count):
+              lj[count] = lj[count]*(xeval - xint[jj])/(xint[count]-xint[jj])
+
+    yeval = 0.
+    
+    for jj in range(N+1):
+       yeval = yeval + yint[jj]*lj[jj]
+  
+    return(yeval)
+  
+def create_natural_spline(yint,xint,N):
+
+#    create the right  hand side for the linear system
+    b = np.zeros(N+1)
+#  vector values
+    h = np.zeros(N+1)  
+    for i in range(1,N):
+        hi = xint[i]-xint[i-1]
+        hip = xint[i+1] - xint[i]
+
+        b[i] = (yint[i+1]-yint[i])/hip - (yint[i]-yint[i-1])/hi
+        h[i-1] = hi
+        h[i] = hip
+
+#  create matrix so you can solve for the M values
+# This is made by filling one row at a time 
+    A = np.zeros((N+1,N+1))
+    A[0][0] = 1.0
+    for j in range(1,N):
+       A[j][j-1] = h[j-1]/6
+       A[j][j] = (h[j]+h[j-1])/3 
+       A[j][j+1] = h[j]/6
+    A[N][N] = 1
+
+    Ainv = inv(A)
+    
+    M  = Ainv.dot(b)
+
+#  Create the linear coefficients
+    C = np.zeros(N)
+    D = np.zeros(N)
+    for j in range(N):
+       C[j] = yint[j]/h[j]-h[j]*M[j]/6
+       D[j] = yint[j+1]/h[j]-h[j]*M[j+1]/6
+    return(M,C,D)
+
+def create_clamped_spline(yint,xint,N):
+
+#    create the right  hand side for the linear system
+    b = np.zeros(N+1)
+#  vector values
+    h = np.zeros(N+1)  
+    for i in range(1,N):
+       hi = xint[i]-xint[i-1]
+       hip = xint[i+1] - xint[i]
+       b[i] = (yint[i+1]-yint[i])/hip - (yint[i]-yint[i-1])/hi
+       h[i-1] = hi
+       h[i] = hip
+
+#  create matrix so you can solve for the M values
+# This is made by filling one row at a time 
+    A = np.zeros((N+1,N+1))
+    A[0][0] = 1.0
+    for j in range(1,N):
+       A[j][j-1] = h[j-1]/6
+       A[j][j] = (h[j]+h[j-1])/3 
+       A[j][j+1] = h[j]/6
+    A[N][N] = 1
+
+    Ainv = inv(A)
+    
+    M  = Ainv.dot(b)
+
+#  Create the linear coefficients
+    C = np.zeros(N)
+    D = np.zeros(N)
+    for j in range(N):
+       C[j] = yint[j]/h[j]-h[j]*M[j]/6
+       D[j] = yint[j+1]/h[j]-h[j]*M[j+1]/6
+    return(M,C,D) 
+
+def eval_local_spline(xeval,xi,xip,Mi,Mip,C,D):
+# Evaluates the local spline as defined in class
+# xip = x_{i+1}; xi = x_i
+# Mip = M_{i+1}; Mi = M_i
+
+    hi = xip-xi
+    yeval = (Mi*(xip-xeval)**3 +(xeval-xi)**3*Mip)/(6*hi) \
+            + C*(xip-xeval) + D*(xeval-xi)
+    return yeval 
+    
+def  eval_cubic_spline(xeval,Neval,xint,Nint,M,C,D):
+    
+    yeval = np.zeros(Neval+1)
+    
+    for j in range(Nint):
+        '''find indices of xeval in interval (xint(jint),xint(jint+1))'''
+        '''let ind denote the indices in the intervals'''
+        atmp = xint[j]
+        btmp= xint[j+1]
+        
+#   find indices of values of xeval in the interval
+        ind= np.where((xeval >= atmp) & (xeval <= btmp))
+        xloc = xeval[ind]
+
+# evaluate the spline
+        yloc = eval_local_spline(xloc,atmp,btmp,M[j],M[j+1],C[j],D[j])
+#        print('yloc = ', yloc)
+#   copy into yeval
+        yeval[ind] = yloc
+
+    return(yeval)
+           
+driver(5,True,True)  
+driver(10,False)
+driver(15,False)
+driver(20,False)             
+
+   
+   
--- Homework/homework8/p1.py
+++ Homework/homework8/p1.py
@@ -1,91 +1,78 @@
-import numpy as np
-from numpy.linalg import inv 
-import matplotlib.pyplot as plt
 
-def driver():
-    f = lambda x: 1/(1+x**2)
-    fp = lambda x: -1/(1+x**2)**2 * 2*x
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy.linalg import inv
+import math
+
+def driver(N):
+
+    f = lambda x: 1./(1.+x**2)
+    fp = lambda x: -2*x/(1.+x**2)**2
+    
+    ''' interval'''
     a = -5
     b = 5
-    num = 100
-    ''' generating nodes'''
-    N = [5,10,15,20]
-    xeval = np.linspace(-5,5,100)
-    xint = [np.linspace(a,b,j) for j in N]
-    yint = [ f(xint[j]) for j in range(4)]
-    ypint = [ fp(xint[j]) for j in range(4)]
-    xintc = [np.cos(((2*np.arange(1, (i)+1) - 1) * np.pi) / (2 * (i))) for i in N]
-    yintc = [ f(xintc[j]) for j in range(4)]
-    ypintc = [ fp(xintc[j]) for j in range(4)]
-
-    # -----------------------------------------------------
-    for i in range(4):
-        # note that this maybe graphs them all on top of eachother
-        Leval = np.zeros(num)
-        Heval = np.zeros(num)
-        for j in range(num):
-            Leval[j] = lagrange(xeval[j],xint[i],yint[i],N[i])
-            Heval[j] = hermite(xeval[j],xint[i],yint[i],ypint[i],N[i])
-
-        (M,C,D) = create_natural_spline(yint[i],xint[i],N[i])
-        nCeval = eval_cubic_spline(xeval,num,xint[i],N[i],M,C,D)
-
-      #  cCeval = 0 # TODO
-        plt.figure(1)
-        plt.title(f'Equispaced Nodes\nn = {N[i]}')
-        plt.plot(xeval,f(xeval))
-        plt.plot(xint[i],yint[i],'o')
-        plt.plot(xeval,Leval,label = "Lagrange")
-        plt.plot(xeval,Heval, label = "Hermite")
-        plt.plot(xeval,nCeval, label = "Natural Cubic spline")
-      #  plt.plot(xeval,cCeval, label = "Clamped Cubic spline")
-        plt.legend()
-        plt.savefig("problem1.{i}.png".format(i=i))
-        
-        Levalc = lagrange(xeval,xintc[i],yintc[i],N[i])
-        Hevalc = hermite(xeval,xintc[i],yintc[i],ypintc[i],N[i])
-        nCevalc = 0 # TODO
-     #   cCevalc = 0 # TODO
-        plt.figure(2)
-        plt.title(f'Chebychev Nodes\nn = {N[i]}')
-        plt.plot(xintc[i],yintc[i],'o')
-        plt.plot(xeval,f(xeval))
-        plt.plot(xeval,Levalc,label = "Lagrange")
-        plt.plot(xeval,Hevalc, label = "Hermite")
-        plt.plot(xeval,nCevalc, label = "Natural Cubic spline")
-     #   plt.plot(xeval,cCevalc, label = "Clamped Cubic spline")
-        plt.savefig("problem2.{i}.png".format(i=i))
-
+   
+    ''' create equispaced interpolation nodes'''
+    xint = np.linspace(a,b,N+1)
     
-def lagrange(xeval,xint,yint,N):
-    lj = np.ones(N+1)
-    for count in range(N):
-       for jj in range(N):
-           if (jj != count):
-              lj[count] = lj[count]*(xeval - xint[jj])/(xint[count]-xint[jj])
-
-    yeval = 0.
+    ''' create interpolation data'''
+    yint = np.zeros(N+1)
+    ypint = np.zeros(N+1)
+    for jj in range(N+1):
+        yint[jj] = f(xint[jj])
+        ypint[jj] = fp(xint[jj])
     
-    for jj in range(N):
-       yeval = yeval + yint[jj]*lj[jj]
-  
-    return(yeval)
+    ''' create points for evaluating the Lagrange interpolating polynomial'''
+    Neval = 1000
+    xeval = np.linspace(a,b,Neval+1)
+    (M,C,D) = create_natural_spline(yint,xint,N)
+    yevalL = np.zeros(Neval+1)
+    yevalH = np.zeros(Neval+1)
+    yevalC = eval_cubic_spline(xeval,Neval,xint,N,M,C,D)
+    for kk in range(Neval+1):
+      yevalL[kk] = eval_lagrange(xeval[kk],xint,yint,N)
+      yevalH[kk] = eval_hermite(xeval[kk],xint,yint,ypint,N)
 
-def hermite(xeval,xint,yint,ypint,N):
+    ''' create vector with exact values'''
+    fex = np.zeros(Neval+1)
+    for kk in range(Neval+1):
+        fex[kk] = f(xeval[kk])
     
+    
+    plt.figure()
+    plt.plot(xeval,fex,label = "f(x)")
+    plt.plot(xeval,yevalL,label='Lagrange') 
+    plt.plot(xeval,yevalH,label='Hermite')
+    plt.plot(xeval,yevalC,label='Natural spline') 
+    plt.legend()
+    plt.semilogy()
+    plt.savefig("problem1.{N}.png".format(N=N))
+
+    ''' 
+    errL = abs(yevalL-fex)
+    errH = abs(yevalH-fex)
+    plt.figure()
+    plt.semilogy(xeval,errL,'bs--',label='Lagrange')
+    plt.semilogy(xeval,errH,'c.--',label='Hermite')
+    plt.show()            
+    '''
+
+def eval_hermite(xeval,xint,yint,ypint,N):
+
     ''' Evaluate all Lagrange polynomials'''
 
     lj = np.ones(N+1)
-    for count in range(N):
-       for jj in range(N):
+    for count in range(N+1):
+       for jj in range(N+1):
            if (jj != count):
               lj[count] = lj[count]*(xeval - xint[jj])/(xint[count]-xint[jj])
 
     ''' Construct the l_j'(x_j)'''
     lpj = np.zeros(N+1)
 #    lpj2 = np.ones(N+1)
-    for count in range(N):
-       for jj in range(N):
+    for count in range(N+1):
+       for jj in range(N+1):
            if (jj != count):
 #              lpj2[count] = lpj2[count]*(xint[count] - xint[jj])
               lpj[count] = lpj[count]+ 1./(xint[count] - xint[jj])
@@ -93,7 +80,7 @@ def hermite(xeval,xint,yint,ypint,N):
 
     yeval = 0.
     
-    for jj in range(N):
+    for jj in range(N+1):
        Qj = (1.-2.*(xeval-xint[jj])*lpj[jj])*lj[jj]**2
        Rj = (xeval-xint[jj])*lj[jj]**2
 #       if (jj == 0):
@@ -108,13 +95,30 @@ def hermite(xeval,xint,yint,ypint,N):
     return(yeval)
        
 
+
+def eval_lagrange(xeval,xint,yint,N):
+
+    lj = np.ones(N+1)
+    
+    for count in range(N+1):
+       for jj in range(N+1):
+           if (jj != count):
+              lj[count] = lj[count]*(xeval - xint[jj])/(xint[count]-xint[jj])
+
+    yeval = 0.
+    
+    for jj in range(N+1):
+       yeval = yeval + yint[jj]*lj[jj]
+  
+    return(yeval)
+  
 def create_natural_spline(yint,xint,N):
 
 #    create the right  hand side for the linear system
     b = np.zeros(N+1)
 #  vector values
     h = np.zeros(N+1)  
-    for i in range(1,N-1):
+    for i in range(1,N):
        hi = xint[i]-xint[i-1]
        hip = xint[i+1] - xint[i]
        b[i] = (yint[i+1]-yint[i])/hip - (yint[i]-yint[i-1])/hi
@@ -142,7 +146,42 @@ def create_natural_spline(yint,xint,N):
        C[j] = yint[j]/h[j]-h[j]*M[j]/6
        D[j] = yint[j+1]/h[j]-h[j]*M[j+1]/6
     return(M,C,D)
-       
+
+def create_clamped_spline(yint,xint,N):
+
+#    create the right  hand side for the linear system
+    b = np.zeros(N+1)
+#  vector values
+    h = np.zeros(N+1)  
+    for i in range(1,N):
+       hi = xint[i]-xint[i-1]
+       hip = xint[i+1] - xint[i]
+       b[i] = (yint[i+1]-yint[i])/hip - (yint[i]-yint[i-1])/hi
+       h[i-1] = hi
+       h[i] = hip
+
+#  create matrix so you can solve for the M values
+# This is made by filling one row at a time 
+    A = np.zeros((N+1,N+1))
+    A[0][0] = 1.0
+    for j in range(1,N):
+       A[j][j-1] = h[j-1]/6
+       A[j][j] = (h[j]+h[j-1])/3 
+       A[j][j+1] = h[j]/6
+    A[N][N] = 1
+
+    Ainv = inv(A)
+    
+    M  = Ainv.dot(b)
+
+#  Create the linear coefficients
+    C = np.zeros(N)
+    D = np.zeros(N)
+    for j in range(N):
+       C[j] = yint[j]/h[j]-h[j]*M[j]/6
+       D[j] = yint[j+1]/h[j]-h[j]*M[j+1]/6
+    return(M,C,D) 
+
 def eval_local_spline(xeval,xi,xip,Mi,Mip,C,D):
 # Evaluates the local spline as defined in class
 # xip = x_{i+1}; xi = x_i
@@ -152,7 +191,6 @@ def eval_local_spline(xeval,xi,xip,Mi,Mip,C,D):
     yeval = (Mi*(xip-xeval)**3 +(xeval-xi)**3*Mip)/(6*hi) \
             + C*(xip-xeval) + D*(xeval-xi)
     return yeval 
-    
     
 def  eval_cubic_spline(xeval,Neval,xint,Nint,M,C,D):
     
@@ -175,10 +213,11 @@ def  eval_cubic_spline(xeval,Neval,xint,Nint,M,C,D):
         yeval[ind] = yloc
 
     return(yeval)
-                      
+           
+driver(5)  
+driver(10)
+driver(15)
+driver(20)             
 
-
-
-def clampedCubic():
-    return 0
-driver()
+   
+   
